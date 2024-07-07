050e3069b85bcced456e2713c8e59e802ec08b7e
mohbscode <mohbscode@gmail.com>
2024-07-07 13:08:56 +0100

    Removed comments and Todo's

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 // import 'dart:math';
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
@@ -49,9 +48,7 @@ class _QuizPageState extends State<QuizPage> {
 
     setState(
       () {
-        //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
         if (end == true) {
-          // TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
           Alert(
             context: context,
             type: AlertType.none,
@@ -75,14 +72,9 @@ class _QuizPageState extends State<QuizPage> {
               )
             ],
           ).show();
-          //TODO: Step 4 Part C - reset the questionNumber,
           quizBrain.reset();
-          //TODO: Step 4 Part D - empty out the scoreKeeper.
           scoreKeeper.clear();
-        }
-
-        //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇too
-        else {
+        } else {
           if (correctAnswer == userChoice) {
             scoreKeeper.add(
               const Icon(
@@ -99,7 +91,6 @@ class _QuizPageState extends State<QuizPage> {
             );
           }
           quizBrain.nextQuestion();
-          print(quizBrain.isFinished());
         }
       },
     );
