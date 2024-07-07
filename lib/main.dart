5154caa8a75ad94f0e66b88d4e52c5c65e3be27e
mohbscode <mohbscode@gmail.com>
2024-07-07 10:54:11 +0100

    created a function to check answers and added todo's for challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // import 'dart:math';
 import 'package:flutter/material.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
 import 'quiz_brain.dart';
 
 // QuizBrain object
@@ -40,6 +41,36 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  void checkAnswer(bool userChoice) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇too
+      if (correctAnswer == userChoice) {
+        scoreKeeper.add(
+          const Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          const Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -67,25 +98,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                bool correctAnswer = quizBrain.getQuestionAnswer();
-                setState(() {
-                  quizBrain.nextQuestion();
-                  if (correctAnswer == true) {
-                    scoreKeeper.add(
-                      const Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ),
-                    );
-                  } else {
-                    scoreKeeper.add(
-                      const Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
-                  }
-                });
+                checkAnswer(true);
               },
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all(Colors.green),
@@ -110,25 +123,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
-                bool correctAnswer = quizBrain.getQuestionAnswer();
-                setState(() {
-                  quizBrain.nextQuestion();
-                  if (correctAnswer == false) {
-                    scoreKeeper.add(
-                      const Icon(
-                        Icons.check,
-                        color: Colors.green,
-                      ),
-                    );
-                  } else {
-                    scoreKeeper.add(
-                      const Icon(
-                        Icons.close,
-                        color: Colors.red,
-                      ),
-                    );
-                  }
-                });
+                checkAnswer(false);
               },
               style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all(Colors.red),
