79068877175f11e19fc9905067bf9661d1c26a59
mohbscode <mohbscode@gmail.com>
2024-07-07 10:17:16 +0100

    updated code for encapsulation

--- lib/main.dart
+++ lib/main.dart
@@ -40,19 +40,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.',
-  // ];
-
-  // List<bool> answers = [false, true, true];
-
-  // Question q1 = Question(
-  //     q: 'You can lead a cow down stairs but not up stairs.', a: false);
-
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -65,8 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                // quizBrain.questionBank[questionNumber].questionText,
-                quizBrain.getQuestionText(questionNumber),
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   color: Colors.white,
@@ -81,12 +67,9 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
+                bool correctAnswer = quizBrain.getQuestionAnswer();
                 setState(() {
-                  bool correctAnswer =
-                      // quizBrain.questionBank[questionNumber].questionAnswer;
-                      quizBrain.getQuestionAnswer(questionNumber);
-                  // questionNumber = Random().nextInt(3);
-
+                  quizBrain.nextQuestion();
                   if (correctAnswer == true) {
                     scoreKeeper.add(
                       const Icon(
@@ -102,8 +85,6 @@ class _QuizPageState extends State<QuizPage> {
                       ),
                     );
                   }
-
-                  questionNumber++;
                 });
               },
               style: ButtonStyle(
@@ -129,12 +110,9 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               onPressed: () {
+                bool correctAnswer = quizBrain.getQuestionAnswer();
                 setState(() {
-                  bool correctAnswer =
-                      // quizBrain.questionBank[questionNumber].questionAnswer;
-                      quizBrain.getQuestionAnswer(questionNumber);
-                  // questionNumber = Random().nextInt(3);
-
+                  quizBrain.nextQuestion();
                   if (correctAnswer == false) {
                     scoreKeeper.add(
                       const Icon(
@@ -150,7 +128,6 @@ class _QuizPageState extends State<QuizPage> {
                       ),
                     );
                   }
-                  questionNumber++;
                 });
               },
               style: ButtonStyle(
